da8c37e8d0edd44334888f693dd7249bd371603f
Raymond Brink <git@rbrink.nl>
2016-02-25 01:32:57 +0100

    + Added flow card to send a command to a specific device.

--- app.js
+++ app.js
@@ -50,11 +50,54 @@ var self = module.exports = {
          * @returns {} true/false
          */
         function turnOff(harmonyClient) {
-            console.log("Turning all devices off...");
             harmonyClient.turnOff();
             harmonyClient.end();
             console.log("All devices turned off.");
             return true;
+        }
+
+        /**
+         * Gets a list of devices, returned through the specified callback method.
+         * 
+         * @param {} harmonyClient 
+         * @param {} callback 
+         * @returns {} 
+         */
+        function getDevices(harmonyClient, callback) {
+            harmonyClient.getAvailableCommands()
+                .then(function(commands) {
+                    var devices = [];
+                    commands.device.forEach(function(device) {
+                        console.log("Found device " + device.id + ": " + device.label);
+                        //device.icon = "";
+                        device.name = device.label;
+                        device.description = device.model;
+                        devices.push(device);
+                    });
+                    harmonyClient.end();
+                    if (callback) callback(devices);
+                });
+        }
+
+        /**
+         * Sends the specified command through the given harmony client.
+         * 
+         * @param {} harmonyClient 
+         * @param {} action 
+         * @returns {} true/false 
+         */
+        function sendAction(harmonyClient, action) {
+            var encodedAction = action.replace(/\:/g, "::");
+            harmonyClient.send("holdAction", "action=" + encodedAction + ":status=press");
+            harmonyClient.end();
+            console.log("Action sent.");
+            return true;
+        }
+
+        Array.prototype.sortBy = function(p) {
+            return this.slice(0).sort(function(a, b) {
+                return (a[p] > b[p]) ? 1 : (a[p] < b[p]) ? -1 : 0;
+            });
         }
 
         Homey.manager("flow").on("action.start_activity", function(callback, args) {
@@ -94,6 +137,7 @@ var self = module.exports = {
         });
 
         Homey.manager("flow").on("action.all_off", function(callback, args) {
+            console.log("Turning all devices off on " + args.hub.ipaddress + "...");
             harmony(args.hub.ipaddress)
                 .then(function(harmonyClient) {
                     harmonyClient.isOff()
@@ -106,6 +150,54 @@ var self = module.exports = {
                             harmonyClient.end();
                         });
                 });
+        });
+
+        Homey.manager("flow").on("action.send_command_to_device", function(callback, args) {
+            console.log("Sending command to " + args.hub.ipaddress + "...");
+            console.log(args);
+            harmony(args.hub.ipaddress)
+                .then(function(harmonyClient) {
+                    var actionSent = sendAction(harmonyClient, args.action.action);
+                    callback(null, actionSent);
+                }).catch(function(e) {
+                    console.log(e);
+                    callback(null, false);
+                });
+        });
+
+        Homey.manager("flow").on("action.send_command_to_device.device.autocomplete", function(callback, args) {
+            console.log(args);
+            // TODO: get hub ip from args.
+            harmony("192.168.2.20")
+                .then(function(harmonyClient) {
+                    getDevices(harmonyClient, function(devices) {
+                        callback(null, devices.sortBy("name"));
+                    });
+                });
+        });
+
+        Homey.manager("flow").on("action.send_command_to_device.controlGroup.autocomplete", function(callback, args) {
+            console.log(args);
+            if (args.device.length === 0) {
+                callback(null, []);
+            }
+
+            callback(null, args.device.controlGroup.sortBy("name"));
+        });
+
+        Homey.manager("flow").on("action.send_command_to_device.action.autocomplete", function(callback, args) {
+            console.log(args);
+            if (args.device.length === 0 || args.controlGroup.length === 0) {
+                callback(null, []);
+            }
+
+            var actions = [];
+            args.controlGroup.function.forEach(function(action) {
+                action.name = action.label;
+                actions.push(action);
+            });
+
+            callback(null, actions.sortBy("name"));
         });
 
         console.log("Listening for triggers...");
